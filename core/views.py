9522d0c257410babddbe052ce7b44c20f8e68b08
Apurva-Jyoti-paul <apurva721034@gmail.com>
2021-07-24 02:06:02 +0530

    Added erron handling to accept the sanbox start post request from gupshupio while setting callback url

--- core/views.py
+++ core/views.py
@@ -11,30 +11,32 @@ from rest_framework import status
 #@csrf_exempt
 @api_view(['POST'])
 def getmessage(request):
-    if request.method=='POST':
-        #data=json.loads(request.body.decode('utf-8'))
-        #data = {'message': request.DATA.get('the_post'), 'author': request.user.pk}
+    try:
+        if request.method=='POST':
+            #data=json.loads(request.body.decode('utf-8'))
+            #data = {'message': request.DATA.get('the_post'), 'author': request.user.pk}
 
-        mesdata=request.data.get('payload')
-        print(mesdata['payload']['text'])
-        databuffer={
-            'mesgcont':mesdata['payload']['text'],
-            'source' : mesdata['source'],
-            'conttype':mesdata['type'],
-            'sender':mesdata['sender']['name'],
-            'countrycode':mesdata['sender']['country_code'],
-            'dialcode': mesdata['sender']['dial_code']
-            }
-        serializer = MessageSerializer(data=databuffer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            mesdata=request.data.get('payload')
+            print(mesdata['payload']['text'])
+            databuffer={
+                'mesgcont':mesdata['payload']['text'],
+                'source' : mesdata['source'],
+                'conttype':mesdata['type'],
+                'sender':mesdata['sender']['name'],
+                'countrycode':mesdata['sender']['country_code'],
+                'dialcode': mesdata['sender']['dial_code']
+                }
+            serializer = MessageSerializer(data=databuffer)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_200_OK)
         #print(mesdata['payload']['text'])
         #return HttpResponse(status=200)
-    else :
-        return HttpResponse(status=400)
- 
+        else :
+            return HttpResponse(status=200)
+    except:
+        return HttpResponse(status=200)
 
 
 
